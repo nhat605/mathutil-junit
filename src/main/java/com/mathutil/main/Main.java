054cb2cf1621690e53593ecaa761ddbaae4d2623
Van Nhat <nhaat605@gmail.com>
2026-01-15 13:22:53 +0700

    first commit

---
+++ src/main/java/com/mathutil/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mathutil.main;
+
+import com.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("5!=" +MathUtil.getFactorial(5));
+    }
+}
---
+++ src/test/java/com/mathutil/core/MathUtilTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mathutil.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MathUtilTest {
+    @Test
+    public void testGetFactorialGivenRightArgumentReturnsWell() {
+        int n =0;
+        long expected = 1;
+        long actual = MathUtil.getFactorial(n);
+        
+        assertEquals(2,MathUtil.getFactorial(1));
+        
+    }
+}
